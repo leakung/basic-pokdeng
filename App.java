941e30d54cbd321230cedb462a166d803d0b6dc7
leakung <leakung@gmail.com>
2024-01-19 02:53:50 +0700

    Reduce code by implemt class

--- App.java
+++ App.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        PokDeng pokDeng;
-        pokDeng = new PokDeng();
+        PokDeng pokDeng = new PokDeng();
+        pokDeng.startGame();
         Scanner sc = new Scanner(System.in);
         System.out.println("\nPress enter for play again or E for Exit");
         while (sc.hasNextLine()) {
@@ -11,9 +11,10 @@ public class App {
             if ((input.equals("E")) || (input.equals("e"))) {
                 break;
             } else {
-                pokDeng = new PokDeng();
+                pokDeng.startGame();
             }
             System.out.println("\nPress enter for play again or E for Exit");
         }
+        sc.close();
     }
 }
--- PokDeng.java
+++ PokDeng.java
@@ -5,165 +5,156 @@ import java.util.Scanner;
 
 public class PokDeng {
     private class Card {
-        String value;
-        String type;
+        String rank;
+        String suit;
 
-        Card(String value, String type) {
-            this.value = value;
-            this.type = type;
+        Card(String rank, String suit) {
+            this.rank = rank;
+            this.suit = suit;
         }
 
         public String toString() {
-            return value + "-" + type;
+            return rank + "-" + suit;
         }
 
         public int getValue() {
-            if ("JQK".contains(value)) {
+            if ("JQK".contains(rank)) {
                 return 10;
-            } else if ("A".equals(value)) {
+            } else if ("A".equals(rank)) {
                 return 1;
             }
-            return Integer.parseInt(value);
+            return Integer.parseInt(rank);
         }
 
-        public String getType() {
-            return type;
-        }
-    }
-
-    ArrayList<Card> deck;
-    ArrayList<Card> dealerHand;
-    int dealerSum;
-    boolean dealerDeng;
-    ArrayList<Card> playerHand;
-    int playerSum;
-    boolean playerDeng;
-    String result;
-
-    PokDeng() {
-        startGame();
-    }
-
-    public void startGame() {
-        // deck
-        buildDeck();
-        shuffleDeck();
-
-        dealerHand = new ArrayList<Card>();
-        dealerSum = 0;
-        dealerDeng = false;
-        playerHand = new ArrayList<Card>();
-        playerSum = 0;
-        playerDeng = false;
-        result = "";
-
-        // play
-        dealCard();
-
-        System.out.print("\nPLAYER : ");
-        System.out.println(playerHand);
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Press C for Call card or enter for Stay");
-        while (sc.hasNextLine()) {
-            String input = sc.nextLine();
-            if ((input.equals("C")) || (input.equals("c"))) {
-                callCard();
-                break;
-            } else {
-                break;
-            }
+        public String getRank() {
+            return rank;
         }
 
-        // cal
-        calCard();
-
-        // compare
-        compareCard();
-
-        System.out.print("\nDEALER : ");
-        System.out.println(dealerHand);
-        System.out.print("PLAYER : ");
-        System.out.println(playerHand);
-        System.out.print("\nRESULT : ");
-        System.out.println(result);
-    }
-
-    public void buildDeck() {
-        deck = new ArrayList<Card>();
-        String[] values = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
-        String[] types = { "C", "D", "H", "S" };
-        for (int j = 0; j < types.length; j++) {
-            for (int i = 0; i < values.length; i++) {
-                Card card = new Card(values[i], types[j]);
-                deck.add(card);
-            }
+        public String getSuit() {
+            return suit;
         }
     }
 
-    public void shuffleDeck() {
-        for (int i = 0; i < deck.size(); i++) {
-            Random random = new Random();
-            int j = random.nextInt(deck.size());
-            Card curCard = deck.get(i);
-            Card ranCard = deck.get(j);
-            deck.set(i, ranCard);
-            deck.set(j, curCard);
-        }
-    }
-
-    public void dealCard() {
+    private class Deck {
+        ArrayList<Card> deck;
         Card card;
-        for (int i = 0; i < 2; i++) {
-            card = deck.remove(deck.size() - 1);
-            playerHand.add(card);
-            card = deck.remove(deck.size() - 1);
-            dealerHand.add(card);
-        }
-    }
 
-    public void calCard() {
-        HashSet<Integer> dealerValue = new HashSet<Integer>();
-        HashSet<String> dealerType = new HashSet<String>();
-        HashSet<Integer> playerValue = new HashSet<Integer>();
-        HashSet<String> playerType = new HashSet<String>();
+        Deck() {
+            deck = new ArrayList<Card>();
+            build();
+        }
 
-        for (int i = 0; i < dealerHand.size(); i++) {
-            dealerSum += dealerHand.get(i).getValue();
-            dealerValue.add(dealerHand.get(i).getValue());
-            dealerType.add(dealerHand.get(i).getType());
-        }
-        for (int i = 0; i < playerHand.size(); i++) {
-            playerSum += playerHand.get(i).getValue();
-            playerValue.add(playerHand.get(i).getValue());
-            playerType.add(playerHand.get(i).getType());
-        }
-        if ((dealerType.size() == 1) || (dealerValue.size() == 1)) {
-            dealerDeng = true;
-        }
-        if ((playerType.size() == 1) || (playerValue.size() == 1)) {
-            playerDeng = true;
-        }
-    }
-
-    public void compareCard() {
-        if ((dealerSum % 10) > (playerSum % 10)) {
-            result = "Dealer win";
-            if (dealerDeng) {
-                result += " with " + dealerHand.size() + " deng";
+        private void build() {
+            String[] ranks = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+            String[] suits = { "C", "D", "H", "S" };
+            for (int j = 0; j < suits.length; j++) {
+                for (int i = 0; i < ranks.length; i++) {
+                    card = new Card(ranks[i], suits[j]);
+                    deck.add(card);
+                }
             }
-        } else if ((dealerSum % 10) < (playerSum % 10)) {
+        }
+
+        public void shuffle() {
+            for (int i = 0; i < deck.size(); i++) {
+                Random random = new Random();
+                int j = random.nextInt(deck.size());
+                Card curCard = deck.get(i);
+                Card ranCard = deck.get(j);
+                deck.set(i, ranCard);
+                deck.set(j, curCard);
+            }
+        }
+
+        public Card callCard() {
+            card = this.deck.remove(deck.size() - 1);
+            return card;
+        }
+
+    }
+
+    private class Hand {
+        ArrayList<Card> hand;
+        int sum;
+        boolean deng;
+        int amount;
+        HashSet<String> rank;
+        HashSet<String> suit;
+
+        Hand() {
+            hand = new ArrayList<Card>();
+            sum = 0;
+            deng = false;
+            amount = 0;
+            rank = new HashSet<String>();
+            suit = new HashSet<String>();
+        }
+
+        public void addCard(Card card) {
+            hand.add(card);
+            amount += 1;
+            sum += card.getValue();
+            rank.add(card.getRank());
+            suit.add(card.getSuit());
+            calDeng();
+        }
+
+        private void calDeng() {
+            if ((rank.size() == 1) || (suit.size() == 1)) {
+                deng = true;
+            } else {
+                deng = false;
+            }
+        }
+    }
+
+    private String compareCard(Hand dealer, Hand player) {
+        String result = "";
+        if ((dealer.sum % 10) > (player.sum % 10)) {
+            result = "Dealer win";
+            if (dealer.deng) {
+                result += " with " + dealer.amount + " deng";
+            }
+        } else if ((dealer.sum % 10) < (player.sum % 10)) {
             result = "Player win";
-            if (playerDeng) {
-                result += " with " + playerHand.size() + " deng";
+            if (player.deng) {
+                result += " with " + player.amount + " deng";
             }
         } else {
             result = "Tie";
         }
+        return result;
     }
 
-    public void callCard() {
-        Card card = deck.remove(deck.size() - 1);
-        playerHand.add(card);
+    public void startGame() {
+
+        Deck deck = new Deck();
+        deck.shuffle();
+
+        Hand dealerHand = new Hand();
+        Hand playerHand = new Hand();
+
+        for (int i = 0; i < 2; i++) {
+            playerHand.addCard(deck.callCard());
+            dealerHand.addCard(deck.callCard());
+        }
+
+        System.out.println(playerHand.hand);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Press C for Call card or enter for Stay");
+        String input = sc.nextLine();
+        if ((input.equals("C")) || (input.equals("c"))) {
+            playerHand.addCard(deck.callCard());
+        }
+
+        if (dealerHand.sum % 10 < 6) {
+            dealerHand.addCard(deck.callCard());
+        }
+        
+        System.out.println(dealerHand.hand);
+        System.out.println(playerHand.hand);
+        System.out.println(compareCard(dealerHand, playerHand));
     }
+
 }
